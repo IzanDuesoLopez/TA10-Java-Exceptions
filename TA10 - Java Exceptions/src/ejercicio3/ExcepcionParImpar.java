66847d648e2ed870aa344861f7eff8c0c4e0a79e
IzanDuesoLopez <izandueso@gmail.com>
2022-07-18 11:56:59 +0200

    Cambios

--- TA10 - Java Exceptions/src/ejercicio3/ExcepcionParImpar.java
+++ TA10 - Java Exceptions/src/ejercicio3/ExcepcionParImpar.java
@@ -1,5 +1,43 @@
 package ejercicio3;
 
-public class ExcepcionParImpar {
+public class ExcepcionParImpar extends Exception{
+	// Atributos
+	private boolean esParOImpar;
+	private final static String EXCEPCION_UNO = "Es par";
+	private final static String EXCEPCION_DOS = "Es impar";
 
+	// Constructores
+	public ExcepcionParImpar() {
+			super();
+		}
+
+	public ExcepcionParImpar(boolean esParOImpar) {
+			super();
+			this.esParOImpar = esParOImpar;
+		}
+
+	// Metodo mostrar mensaje excepcion
+	public String getMessage() {
+
+		int resultante = 0;
+		String resultado = ""; // Almacenamos el mensaje resultante
+		
+		if(this.esParOImpar) {
+			resultante = 1;
+		}
+		
+		// Switch con el codigo de la excepcion
+		switch (resultante) {
+		// Comprobamos los diferentes casos
+		case 1:
+			resultado = EXCEPCION_UNO;
+			break;
+		case 0:
+			resultado = EXCEPCION_DOS;
+			break;
+		}
+
+		// Devolvemos el mensaje resultante
+		return resultado;
+	}
 }
