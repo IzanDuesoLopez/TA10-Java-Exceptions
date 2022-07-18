981e46041876813c0e54bc1f222c47102195d048
IzanDuesoLopez <izandueso@gmail.com>
2022-07-18 09:36:10 +0200

    Método generar número random, constructor, atributos, getters y setters, toString

--- TA10 - Java Exceptions/src/ejercicio1/Numero.java
+++ TA10 - Java Exceptions/src/ejercicio1/Numero.java
@@ -1,17 +1,56 @@
 package ejercicio1;
 
+import java.util.Random;
+
 public class Numero {
 	
 	// Atributos
-	
+	private int numero;
+	private int numeroIntentos;
 	
 	// Constructor
+	public Numero() {
+		this.numero = numeroAleatorio();
+		this.numeroIntentos = 0;
+	}
 	
 	// Metodo que genere número entre 1 y 500
+	public int numeroAleatorio() {
+		int minimo = 1;
+		int maximo = 500;
+		Random numRandom = new Random();
+		
+		int numeroAleatorio = numRandom.nextInt((maximo - minimo) + 1) + minimo;
+		
+		return numeroAleatorio;
+	}
 	
 	// Método adivinarNumero
 		// Comprobar si es un número, si no lo es incrementar resultados. Utilizar la Excepcion InputMismatchException
 	
 	// Método conseguirAdivinarlo
+	
+	// Getters y Setters
+	public int getNumero() {
+		return numero;
+	}
 
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getNumeroIntentos() {
+		return numeroIntentos;
+	}
+
+	public void setNumeroIntentos(int numeroIntentos) {
+		this.numeroIntentos = numeroIntentos;
+	}
+
+	// Método toString
+	@Override
+	public String toString() {
+		return "Numero [numero=" + numero + ", numeroIntentos=" + numeroIntentos + "]";
+	}
+	
 }
