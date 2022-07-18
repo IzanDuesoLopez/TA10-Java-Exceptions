a3d73e815c3f1b00fd01da76cfde5782215c2951
IzanDuesoLopez <izandueso@gmail.com>
2022-07-18 11:35:29 +0200

    Distribución trabajo

--- TA10 - Java Exceptions/src/ejercicio3/ParImpar.java
+++ TA10 - Java Exceptions/src/ejercicio3/ParImpar.java
@@ -2,4 +2,14 @@ package ejercicio3;
 
 public class ParImpar {
 
+	// Atributos
+	
+	// Constructores
+	
+	// método generarNumAleatorio()
+	
+	// método comprobarEsParOImpar()
+	
+	// método capturaNumeroComprobarExcepcion()
+	
 }
