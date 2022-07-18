402a702277bea4c6c4eef41b0cdd46486fa2218a
IzanDuesoLopez <izandueso@gmail.com>
2022-07-18 12:12:54 +0200

    Inicialización

--- TA10 - Java Exceptions/src/ejercicio4/Calculos.java
+++ TA10 - Java Exceptions/src/ejercicio4/Calculos.java
@@ -1,5 +1,30 @@
 package ejercicio4;
 
+import java.util.Scanner;
+
 public class Calculos {
 
+	// Atributos
+	Scanner sc = new Scanner(System.in);
+	private double a;
+	private double b;
+	
+	// Constructores
+	
+	// suma
+	
+	// resta
+	
+	// multiplicación
+	
+	// potencia
+	
+	// raíz cuadrada
+	
+	// raíz cubica
+	
+	// raiz
+	
+	// división
+	
 }
