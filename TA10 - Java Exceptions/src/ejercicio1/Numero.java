4873851fe968f3912b2facb175b8ff7556e4d943
IzanDuesoLopez <izandueso@gmail.com>
2022-07-18 10:27:07 +0200

    Correciones

--- TA10 - Java Exceptions/src/ejercicio1/Numero.java
+++ TA10 - Java Exceptions/src/ejercicio1/Numero.java
@@ -5,57 +5,53 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Numero {
-	
+
 	// Atributos
 	private int numero;
 	private int numeroIntentos;
-	
+
 	// Constructor
 	public Numero() {
 		this.numero = numeroAleatorio();
 		this.numeroIntentos = 0;
 	}
-	
+
 	// Metodo que genere número entre 1 y 500
 	public int numeroAleatorio() {
 		int minimo = 1;
 		int maximo = 500;
 		Random numRandom = new Random();
-		
+
 		int numeroAleatorio = numRandom.nextInt((maximo - minimo) + 1) + minimo;
-		
+
 		return numeroAleatorio;
 	}
-	
+
 	// Método adivinarNumero
-		// Comprobar si es un número, si no lo es incrementar resultados. Utilizar la Excepcion InputMismatchException
-<<<<<<< HEAD
-	// Método adivinarNumero
-	// Comprobar si es un número, si no lo es incrementar resultados. Utilizar la Excepcion InputMismatchException
+	// Comprobar si es un número, si no lo es incrementar resultados. Utilizar la
+	// Excepcion InputMismatchException
 	public boolean adivinarNumero() {
 		Scanner sc = new Scanner(System.in);
 		int numero = -1;
 
-		while(numero != this.numero) {
-			System.out.println("Escribir número:");
+		System.out.println("Escribir número:");
 
-			try {
+		try {
+			while (numero != this.numero) {
 				numero = sc.nextInt();
-			} catch(InputMismatchException e) {
-				System.out.println("Eso no es un número");			
+				if (this.numero < numero) {
+					System.out.println("El número es más pequeño.");
+					numeroIntentos++;
+				} else if (this.numero > numero) {
+					System.out.println("El número es más grande.");
+					numeroIntentos++;
+				} else if (this.numero == numero) {
+					conseguirAdivinarlo();
+					break;
+				}
 			}
-
-
-			if(this.numero < numero) {
-				System.out.println("El número es más pequeño.");
-				numeroIntentos++;
-			} else if(this.numero > numero) {
-				System.out.println("El número es más grande.");
-				numeroIntentos++;
-			} else if(this.numero == numero) {
-				System.out.println("El número es correcto.");
-				break;
-			}
+		} catch (InputMismatchException e) {
+			System.out.println("Eso no es un número");
 		}
 
 		sc.close();
@@ -63,17 +59,13 @@ public class Numero {
 		return true;
 	}
 
-
-=======
-	
-	
->>>>>>> branch 'main' of https://github.com/IzanDuesoLopez/TA10-Java-Exceptions.git
 	// Método conseguirAdivinarlo
 	public void conseguirAdivinarlo() {
-		System.out.println("Felicidades, conseguiste adivinar el número" + "Tú número de intentos ha sido: " + numeroIntentos);
-		
+		System.out.println(
+				"Felicidades, conseguiste adivinar el número" + "Tú número de intentos ha sido: " + this.numeroIntentos);
+
 	}
-	
+
 	// Getters y Setters
 	public int getNumero() {
 		return numero;
@@ -96,5 +88,5 @@ public class Numero {
 	public String toString() {
 		return "Numero [numero=" + numero + ", numeroIntentos=" + numeroIntentos + "]";
 	}
-	
+
 }
