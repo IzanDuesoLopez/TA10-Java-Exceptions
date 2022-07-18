6b76da2c80f21e381a453267754e3c5879d4d65b
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-18 11:14:36 +0200

    Commit ex 2

--- TA10 - Java Exceptions/src/ejercicio2/ExcepcionMainApp.java
+++ TA10 - Java Exceptions/src/ejercicio2/ExcepcionMainApp.java
@@ -7,10 +7,9 @@ public class ExcepcionMainApp {
 		int num;
 		try {
 			num = 91;
-			
-			throw new ExcepcionCustomizada(1);
-			//throw new ExcepcionCustomizada(2);
-			//throw new ExcepcionCustomizada(3);
+			if (num >= 100) { throw new ExcepcionCustomizada(1); }
+			else if (num <= 90) { throw new ExcepcionCustomizada(2); }
+			else if (num == 91 ) { throw new ExcepcionCustomizada(3); }
 
 		} catch (ExcepcionCustomizada ex) {
 			System.out.println(ex.getMessage());
