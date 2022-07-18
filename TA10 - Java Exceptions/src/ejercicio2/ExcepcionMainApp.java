0ae0b96c0d2a1cfd08580e50226f18715672e790
IzanDuesoLopez <izandueso@gmail.com>
2022-07-18 11:00:48 +0200

    Recomendaciones

--- TA10 - Java Exceptions/src/ejercicio2/ExcepcionMainApp.java
+++ TA10 - Java Exceptions/src/ejercicio2/ExcepcionMainApp.java
@@ -7,7 +7,9 @@ public class ExcepcionMainApp {
 		int num;
 		try {
 			num = 91;
-			throw new ExcepcionCustomizada(num);
+			throw new ExcepcionCustomizada(1);
+			//throw new ExcepcionCustomizada(2);
+			//throw new ExcepcionCustomizada(3);
 
 		} catch (ExcepcionCustomizada ex) {
 			System.out.println(ex.getMessage());
