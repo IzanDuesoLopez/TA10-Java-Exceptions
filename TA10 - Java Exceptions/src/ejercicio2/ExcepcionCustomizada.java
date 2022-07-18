b024db31fc1fcc739abbc04a25a6cbb6eb6f93c2
IzanDuesoLopez <izandueso@gmail.com>
2022-07-18 10:48:55 +0200

    Método getMessage

--- TA10 - Java Exceptions/src/ejercicio2/ExcepcionCustomizada.java
+++ TA10 - Java Exceptions/src/ejercicio2/ExcepcionCustomizada.java
@@ -6,6 +6,30 @@ public class ExcepcionCustomizada {
 	
 	// Constructores
 	
-	// Metodo mostrar mensaje excepcion
 	
+	// Metodo mostrar mensaje excepcion
+	public String getMessage() {
+		
+		String resultado = ""; // Almacenamos el mensaje resultante
+		
+		// Switch con el codigo de la excepcion
+		switch(codigoExcepcion) {
+		// Comprobamos los diferentes casos
+		case 1:
+			resultado = "";
+			break;
+		case 2:
+			resultado = "";
+			break;
+		case 3:
+			resultado = "";
+			break;
+		default:
+			resultado = "";
+			break;
+		}
+		
+		// Devolvemos el mensaje resultante
+		return resultado;
+	}
 }
