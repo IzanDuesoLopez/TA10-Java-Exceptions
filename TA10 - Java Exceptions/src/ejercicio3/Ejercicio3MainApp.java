cdc4b3dc743a59bea8c6f603a60f2a30dc5cfff7
IzanDuesoLopez <izandueso@gmail.com>
2022-07-18 12:51:52 +0200

    Comentarios

--- TA10 - Java Exceptions/src/ejercicio3/Ejercicio3MainApp.java
+++ TA10 - Java Exceptions/src/ejercicio3/Ejercicio3MainApp.java
@@ -3,12 +3,12 @@ package ejercicio3;
 public class Ejercicio3MainApp {
 
 	public static void main(String[] args) {
-		ParImpar par_impar = new ParImpar();
+		ParImpar par_impar = new ParImpar(); // Creamos una nueva instancia de la clase ParImpar
 		
-		par_impar.esParOImpar();
+		par_impar.esParOImpar(); // Comprobamos si es par o impar
 		System.out.println("Generando número aleatorio...");
-		System.out.println("El número aleatorio generado es: " + par_impar.getNumero());
-		par_impar.capturaNumeroComprobarExcepcion();
+		System.out.println("El número aleatorio generado es: " + par_impar.getNumero()); // Mostramos por pantalla el número generado aleatoriamente
+		par_impar.capturaNumeroComprobarExcepcion(); // Mostramos la excepcion comprobando si es par o impar
 	}
 
 }
