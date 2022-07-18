31adb18528c7cea0062d9e9dc8f2b9a11f842533
daniflorialopez <daniflorialopez@gmail.com>
2022-07-18 12:09:17 +0200

    Main

--- TA10 - Java Exceptions/src/ejercicio3/Ejercicio3MainApp.java
+++ TA10 - Java Exceptions/src/ejercicio3/Ejercicio3MainApp.java
@@ -3,8 +3,12 @@ package ejercicio3;
 public class Ejercicio3MainApp {
 
 	public static void main(String[] args) {
+		ParImpar par_impar = new ParImpar();
 		
-
+		par_impar.esParOImpar();
+		System.out.println("Generando número aleatorio...");
+		System.out.println("El número aleatorio generado es: " + par_impar.getNumero());
+		par_impar.capturaNumeroComprobarExcepcion();
 	}
 
 }
