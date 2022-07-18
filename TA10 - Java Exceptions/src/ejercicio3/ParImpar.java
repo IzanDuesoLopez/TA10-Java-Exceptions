77523851a87440e5fdcfd5139b245cf34a3fafd4
IzanDuesoLopez <izandueso@gmail.com>
2022-07-18 11:53:57 +0200

    Cambios

--- TA10 - Java Exceptions/src/ejercicio3/ParImpar.java
+++ TA10 - Java Exceptions/src/ejercicio3/ParImpar.java
@@ -6,33 +6,35 @@ public class ParImpar {
 
 	// Atributos
 	private int numero;
-	
+
 	// Constructores
 	public ParImpar() {
 		this.numero = 0;
 	}
-	
+
 	// método generarNumAleatorio()
-	
+
 	public int numeroAleatorio() {
 		int minimo = 0;
-		int maximo =999;
+		int maximo = 999;
 		Random numRandom = new Random();
 
 		int numeroAleatorio = numRandom.nextInt((maximo - minimo) + 1) + minimo;
 
 		return numeroAleatorio;
 	}
-	
+
 	// método comprobarEsParOImpar()
 	public boolean esParOImpar() {
-		if(this.numero % 2 == 0) { // Si es divisible entre 2 es par
-			return true;	
+		if (this.numero % 2 == 0) { // Si es divisible entre 2 es par
+			return true;
 		} else { // Si no lo es es impar
 			return false;
 		}
 	}
-	
+
 	// método capturaNumeroComprobarExcepcion()
-	
+	public void capturaNumeroComprobarExcepcion() {
+		
+	}
 }
