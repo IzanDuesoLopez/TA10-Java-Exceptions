6f87ee8029f887162b5006821f65a681d46c6b2b
IzanDuesoLopez <izandueso@gmail.com>
2022-07-18 11:16:49 +0200

    Final

--- TA10 - Java Exceptions/src/ejercicio2/ExcepcionMainApp.java
+++ TA10 - Java Exceptions/src/ejercicio2/ExcepcionMainApp.java
@@ -3,17 +3,24 @@ package ejercicio2;
 public class ExcepcionMainApp {
 
 	public static void main(String[] args) {
+		funcionalidad(); // Llamamos al método funcionalidad del programa
+	}
 
+	// Método en el que lanzamos las diferentes excepciones customizadas
+	public static void funcionalidad() {
 		int num;
 		try {
 			num = 91;
-			if (num >= 100) { throw new ExcepcionCustomizada(1); }
-			else if (num <= 90) { throw new ExcepcionCustomizada(2); }
-			else if (num == 91 ) { throw new ExcepcionCustomizada(3); }
+			if (num >= 100) {
+				throw new ExcepcionCustomizada(1); // Lanzamos la excepción customizada con el mensaje 1
+			} else if (num <= 90) {
+				throw new ExcepcionCustomizada(2); // Lanzamos la excepción customizada con el mensaje 2
+			} else if (num == 91) {
+				throw new ExcepcionCustomizada(3); // Lanzamos la excepción customizada con el mensaje 3
+			}
 
 		} catch (ExcepcionCustomizada ex) {
 			System.out.println(ex.getMessage());
 		}
-
 	}
 }
