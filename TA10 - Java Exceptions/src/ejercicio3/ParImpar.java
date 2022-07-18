f3c3b0e24a0ecd6250d102acdc0a1d407e9864e4
IzanDuesoLopez <izandueso@gmail.com>
2022-07-18 11:49:49 +0200

    esPar()

--- TA10 - Java Exceptions/src/ejercicio3/ParImpar.java
+++ TA10 - Java Exceptions/src/ejercicio3/ParImpar.java
@@ -3,12 +3,23 @@ package ejercicio3;
 public class ParImpar {
 
 	// Atributos
+	private int numero;
 	
 	// Constructores
+	public ParImpar() {
+		this.numero = 0;
+	}
 	
 	// método generarNumAleatorio()
 	
 	// método comprobarEsParOImpar()
+	public boolean esParOImpar() {
+		if(this.numero % 2 == 0) { // Si es divisible entre 2 es par
+			return true;	
+		} else { // Si no lo es es impar
+			return false;
+		}
+	}
 	
 	// método capturaNumeroComprobarExcepcion()
 	
