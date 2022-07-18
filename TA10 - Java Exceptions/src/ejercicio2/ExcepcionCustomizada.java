9f66c59a7019f803e3ded51eaaf2aba8fda1c95a
daniflorialopez <daniflorialopez@gmail.com>
2022-07-18 10:52:29 +0200

    Prueba

--- TA10 - Java Exceptions/src/ejercicio2/ExcepcionCustomizada.java
+++ TA10 - Java Exceptions/src/ejercicio2/ExcepcionCustomizada.java
@@ -1,11 +1,42 @@
 package ejercicio2;
 
-public class ExcepcionCustomizada {
+public class ExcepcionCustomizada extends Exception{
 	
 	// Atributos
+	private int codigo;
 	
 	// Constructores
+	public ExcepcionCustomizada() {
+		super();
+	}
 	
+	public ExcepcionCustomizada(int codigo) {
+		super();
+		this.codigo = codigo;
+	}
 	// Metodo mostrar mensaje excepcion
-	
+	public String getMessage() {
+		
+		String resultado = ""; // Almacenamos el mensaje resultante
+		
+		// Switch con el codigo de la excepcion
+		switch(codigoExcepcion) {
+		// Comprobamos los diferentes casos
+		case 1:
+			resultado = "";
+			break;
+		case 2:
+			resultado = "";
+			break;
+		case 3:
+			resultado = "";
+			break;
+		default:
+			resultado = "";
+			break;
+		}
+		
+		// Devolvemos el mensaje resultante
+		return resultado;
+	}
 }
