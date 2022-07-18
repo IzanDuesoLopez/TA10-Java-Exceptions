b1b1bdcd792519ae98d6568c8c31a49806b0da55
IzanDuesoLopez <izandueso@gmail.com>
2022-07-18 09:26:30 +0200

    Comments

--- TA10 - Java Exceptions/src/ejercicio1/Numero.java
+++ TA10 - Java Exceptions/src/ejercicio1/Numero.java
@@ -1,5 +1,17 @@
 package ejercicio1;
 
 public class Numero {
-
+	
+	// Atributos
+	
+	
+	// Constructor
+	
+	// Metodo que genere número entre 1 y 500
+	
+	// Método adivinarNumero
+		// Comprobar si es un número, si no lo es incrementar resultados. Utilizar la Excepcion InputMismatchException
+	
+	// Método conseguirAdivinarlo
+	
 }
