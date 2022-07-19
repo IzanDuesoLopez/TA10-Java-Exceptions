934eed3f162bc91774d5bab929589389a3e65efa
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-19 22:51:24 +0200

    Commit ex 10

--- TA10 - Java Exceptions/src/ejercicio5/PasswordMainApp.java
+++ TA10 - Java Exceptions/src/ejercicio5/PasswordMainApp.java
@@ -14,6 +14,17 @@ public class PasswordMainApp {
 		cantidad = sc.nextInt();
 		contrasenas = new Password[cantidad];
 		
+		/* Esto es para q lo mireis, a ver q tal
+		 *  // Para generar contraseñas
+				for(i = 0; i < pw.length; i++) {
+					System.out.println("Di el tamaño del la password numero " + (i+1)); // Pedimos la longitud
+					contasenas[i] = new Password(scn.nextInt()); // La generamos
+					fuerte[i] = pw[i].esFuerte(); // Guardamos si es fuerte o no
+				}
+				
+				boolean[] fuerte = new boolean[TAMAÑO]; (habría añadir esto antes)
+		*/
+		
 		// Indicamos longitud de las Passwd antes del bucle
 		
 		// Bucle que crea un objeto por cada posición del array
@@ -25,7 +36,7 @@ public class PasswordMainApp {
 		// Array de booleanos donde decimos si la passwd es fuerte o no
 		
 		// Finalmente mostramos la contraseña y si es fuerte o no
-		for(int i = 0; i < pw.length; i++) {
+		for(int i = 0; i < contrasenas.length; i++) {
 			System.out.println("Contraseña " + (i + 1) + ": " + contrasenas[i].getContrasena() + " " + fuerte[i]);
 		}
 	}
