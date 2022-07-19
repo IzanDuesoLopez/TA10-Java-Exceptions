611b9822f5bbf724bcb796a8d85533d9ddec35a8
IzanDuesoLopez <izandueso@gmail.com>
2022-07-19 21:08:19 +0200

    Planificación clases

--- TA10 - Java Exceptions/src/ejercicio5/Password.java
+++ TA10 - Java Exceptions/src/ejercicio5/Password.java
@@ -1,5 +1,21 @@
 package ejercicio5;
 
 public class Password {
-
+	// Atributos
+	private int longitud;
+	private String contraseña;
+	
+	// Constructor por defecto. Longitud = 8
+	
+	// Constructor con la longitud que le pasemos. Genera contraseña aleatoria
+	
+	/* Método esFuerte(). Devuelve un booleano si es fuerte o no. Para que sea
+	   fuerte debe tener mas de 2 mayusculas, mas de 1 minuscula y mas de 5 números */
+	
+	/* Método generarPassword() genera la contraseña del objeto con 
+	   la longitud que tengamos  */
+	
+	// Getters de contraseña y longitud
+	
+	// Setter para la longitud
 }
---
+++ TA10 - Java Exceptions/src/ejercicio5/PasswordMainApp.java
@@ -0,0 +1,19 @@
+package ejercicio5;
+
+public class PasswordMainApp {
+
+	public static void main(String[] args) {
+
+		// Array de contraseñas con tamaño por teclado
+		
+		// Bucle que crea un objeto por cada posición del array
+		
+		// Indicamos longitud de las Passwd antes del bucle
+		
+		// Array de booleanos donde decimos si la passwd es fuerte o no
+		
+		// Finalmente mostramos la contraseña y si es fuerte o no
+
+	}
+
+}
