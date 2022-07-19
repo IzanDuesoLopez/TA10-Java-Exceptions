a1493b9907cbf8cb9427e209a0c7ce72bcc73db1
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-19 22:46:23 +0200

    Commit ex 5

--- TA10 - Java Exceptions/src/ejercicio5/PasswordMainApp.java
+++ TA10 - Java Exceptions/src/ejercicio5/PasswordMainApp.java
@@ -25,7 +25,9 @@ public class PasswordMainApp {
 		// Array de booleanos donde decimos si la passwd es fuerte o no
 		
 		// Finalmente mostramos la contraseña y si es fuerte o no
-
+		for(int i = 0; i < pw.length; i++) {
+			System.out.println("Contraseña " + (i + 1) + ": " + contrasenas[i].getContrasena() + " " + fuerte[i]);
+		}
 	}
 
 }
