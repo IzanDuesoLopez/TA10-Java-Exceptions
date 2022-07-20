d65cac51faee573bb668b97a2aacf2ee98cedda1
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-20 15:27:57 +0200

    Commit ex 5 TA 10

--- TA10 - Java Exceptions/src/ejercicio5/PasswordMainApp.java
+++ TA10 - Java Exceptions/src/ejercicio5/PasswordMainApp.java
@@ -14,28 +14,33 @@ public class PasswordMainApp {
 		cantidad = sc.nextInt();
 		contrasenas = new Password[cantidad];
 
-		// TEST 
-		Password contra = new Password(20);
-		System.out.println("Contraseña: " + contra.getContrasena());
-		if(contra.esFuerte()) {
-			System.out.println("Es fuerte");
-		} else {
-			System.out.println("No es fuerte");
-		}
-		// TEST 
-
 		// Indicamos longitud de las Passwd antes del bucle
+		System.out.println("Introduzca la longitud de la contraseña a generar:");
+		int longitud = sc.nextInt();
+		sc.nextLine();
 
 		// Bucle que crea un objeto por cada posición del array
 		for (int i = 0; i < cantidad; i++) {
-			// contrasenas[i] = new Password(longitud);
+			contrasenas[i] = new Password(longitud);
 		}
 
 		// Array de booleanos donde decimos si la passwd es fuerte o no
+		boolean[] fuerte = new boolean[cantidad];
+		for(int i = 0; i <  fuerte.length; i++) {
+			fuerte[i] = contrasenas[i].esFuerte();
+		}
 
 		// Finalmente mostramos la contraseña y si es fuerte o no
-		// for(int i = 0; i < pw.length; i++) {
-		// System.out.println("Contraseña " + (i + 1) + ": " +
-		// contrasenas[i].getContrasena() + " " + fuerte[i]);
+		for(int i = 0; i < contrasenas.length; i++) {
+			boolean resultado = contrasenas[i].esFuerte();
+			if(resultado) {
+				System.out.println("Es fuerte");
+			} else {
+				System.out.println("No es fuerte");
+			}
+			System.out.println("Contraseña " + (i + 1) + ": " +
+			contrasenas[i].getContrasena() + " " + resultado);
+		}
+		sc.close();
 	}
 }
